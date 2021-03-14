39906916ed81d92324dfc23c20c4c07512084d30
Joel DeSante <joeldesante@gmail.com>
2021-03-14 15:53:27 -0400

    Cleaned up the fish class a little

--- MaritimeTravel/Source/GameObjects/Fish.cs
+++ MaritimeTravel/Source/GameObjects/Fish.cs
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework.Input;
 
 namespace MaritimeTravel.Source.GameObjects {
     class Fish : GameObject {
+
         private Transform transform;
         private Sprite fishSprite;
         private Rigidbody physics;
@@ -15,11 +16,10 @@ namespace MaritimeTravel.Source.GameObjects {
 
         public Fish(Texture2D fishTexture) {
             this.fishSprite = new Sprite(fishTexture, new Vector2(fishTexture.Width / 2, fishTexture.Height / 2));
-            transform = new Transform();
-            physics = new Rigidbody(30);
-            health = new Health();
-
-            transform.Scale *= 5;
+            this.transform = new Transform();
+            this.physics = new Rigidbody(30);
+            this.health = new Health();
+            this.transform.Scale *= 3;
         }
 
         public override void Draw(SpriteBatch spriteBatch, GameTime gameTime) {
